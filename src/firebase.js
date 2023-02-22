3def724e55ee192b7e4960fcf0accbb5f174e3c3
Son Haechang <sonhaechang@Sonui-MacBookPro.local>
2023-02-22 16:06:09 +0900

    build: SetUp firebase
    1. SetUp firebase
    2. firebase.js 생성
    3. package-lock.json 및 package.json에 firebase 관련 추가

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAwwK_AwiBchINkqkC4eNkdNc0VCVlSKO4",
+    authDomain: "nomadcoder-twitter.firebaseapp.com",
+    projectId: "nomadcoder-twitter",
+    storageBucket: "nomadcoder-twitter.appspot.com",
+    messagingSenderId: "395086529409",
+    appId: "1:395086529409:web:dca151ff7c1875f592de80"
+};
+
+export default initializeApp(firebaseConfig);
