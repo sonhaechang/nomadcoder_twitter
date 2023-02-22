aa69af7fc78064835fa30e96a4a99db1daccd2a6
Son Haechang <sonhaechang@Sonui-MacBookPro.local>
2023-02-22 16:21:37 +0900

    refector: create env
    1. env로 환경변수 관리
    2. .gitignoredp env 추가

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAwwK_AwiBchINkqkC4eNkdNc0VCVlSKO4",
-    authDomain: "nomadcoder-twitter.firebaseapp.com",
-    projectId: "nomadcoder-twitter",
-    storageBucket: "nomadcoder-twitter.appspot.com",
-    messagingSenderId: "395086529409",
-    appId: "1:395086529409:web:dca151ff7c1875f592de80"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
 };
 
 export default initializeApp(firebaseConfig);
