a32ad5f31086198d3913e4335e34466ceac121bd
Son Haechang <sonhaechang@Sonui-MacBookPro.local>
2023-02-27 14:12:43 +0900

    feat: Using Firebase Auth
    1. firebase.js -> fbase.js로 변경 및 getAuth() import후 export하게 수정
    2. AppRouter.js에 isLoggedIn useSatate 제거 및 isLoggedIn 상태를 props로 받아오게 수정
    3. authService를 import후 isLoggedIn useState 추가 및 default값으로 authService.currentUser 설정(이 값을 AppRouter에 props 값으로 전달 및 footer생성)
    4. jsconfig.json 생성(경로 관련 설정)
    5. index.js에 <React.StrictMode> 주석처리

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,15 @@
-import React from 'react';
-import AppRouter from './AppRouter';
+import React, { useState } from 'react';
+import AppRouter from 'components/AppRouter';
+import {authService} from 'fbase';
 
 function App() {
+	const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+
 	return (
-		<AppRouter/>
+		<>
+			<AppRouter isLoggedIn={isLoggedIn} />
+			<footer>&copy; {new Date().getFullYear()} Nwitter</footer>
+		</>
 	);
 }
 
--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -1,11 +1,8 @@
-import React, { useState } from 'react';
 import { HashRouter, Routes, Route } from 'react-router-dom';
 import Auth from '../routes/Auth';
 import Home from '../routes/Home';
 
-function AppRouter() {
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
-
+function AppRouter({ isLoggedIn }) {
     return (
         <HashRouter>
             <Routes>
