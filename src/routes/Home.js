9e4b6b62f1eb1163237bda48975f6740783e4ef9
Son Haechang <sonhaechang@Sonui-MacBookPro.local>
2023-02-28 17:03:04 +0900

    feat: Handling get requests using firebase's firestore
    1. getNweets function 생성 (getDocs를 사용해서 get으로 읽어온 값 setNweets에 할당하는 기능)
    2. 가져온 값(nweets)를 map 함수로 반복해서 jsx 생성

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,23 +1,44 @@
 import { dbService } from 'fbase';
-import { addDoc, collection } from 'firebase/firestore';
-import React, { useState } from 'react';
+import { addDoc, collection, getDocs } from 'firebase/firestore';
+import React, { useEffect, useState } from 'react';
 
 function Home() {
     const [nweet, setNweet] = useState('');
+    const [nweets, setNweets] = useState([]);
+
+    const getNweets = async () => {
+        const dbNweets = await getDocs(collection(dbService, 'nweets'));
+
+        dbNweets.forEach(doc => {
+            const nweetObject = {
+                ...doc.data(),
+                id: doc.id
+            }
+
+            setNweets((prev) => [nweetObject, ...prev]);
+        });
+    };
 
     const onSubmit = async (e) => {
         e.preventDefault();
+
         await addDoc(collection(dbService, 'nweets'), {
             nweet,
             createdAt: Date.now(),
         });
+
         setNweet('');
-    }
+    };
 
     const onChange = (e) => {
         const { target: {value} } = e;
+        
         setNweet(value);
-    }
+    };
+
+    useEffect(() => {
+        getNweets();
+    }, []);
 
     return (
         <div>
@@ -34,8 +55,15 @@ function Home() {
                     value='Nweet' 
                 />
             </form>
+            <div>
+                {nweets.map(nweet => 
+                    <div key={nweet.id}>
+                        <h4>{nweet.nweet}</h4>
+                    </div>
+                )}
+            </div>
         </div>
     );
-}
+};
 
 export default Home;
