47a94bfb5f5d75be91c82779aa4b0d1c13745d77
Son Haechang <sonhaechang@Sonui-MacBookPro.local>
2023-02-28 14:00:33 +0900

    feat: Logout
    1. Profile.js에서 로그아웃 기능 생성
    2. Navigation.js 생성(Home, My Profile Link 추가)
    3. AppRouter.js에서 Navigation 및 Profile 추가

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -1,15 +1,20 @@
 import { HashRouter, Routes, Route } from 'react-router-dom';
-import Auth from '../routes/Auth';
-import Home from '../routes/Home';
+import Profile from 'routes/Profile';
+import Auth from 'routes/Auth';
+import Home from 'routes/Home';
+import Navigation from 'components/Navigation';
 
 function AppRouter({ isLoggedIn }) {
     return (
         <HashRouter>
+            {isLoggedIn && <Navigation />}
+
             <Routes>
                 {
                     isLoggedIn ? 
                     <>
                         <Route path='/' element={<Home />} />
+                        <Route path='/profile' element={<Profile />} />
                     </> 
                     : 
                     <Route path='/' element={<Auth />} />
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,8 +1,21 @@
+import { authService } from 'fbase';
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function Profile() {
+    const navigate = useNavigate();
+
+    const onLogOutClick = () => {
+        authService.signOut();
+        navigate('/');
+    }
+    
     return (
-        <span>Profile</span>
+        <>
+            <button onClick={onLogOutClick}>
+                Logout
+            </button>
+        </>
     );
 }
 
