01b7b81f392249ab2ae542dbccbb68260d15f221
Son Haechang <sonhaechang@Sonui-MacBookPro.local>
2023-02-28 20:13:36 +0900

    feat: Delete and Update part Two
    1. firebase의 deleteDoc을 이용한 삭제기능 추가(onDeleteClick)
    2. firebase의 updateDoc을 이용한 수정기능 추가(onChange, onSubmit)
    3. 수정 form jxs 추가

--- src/components/Nweet.js
+++ src/components/Nweet.js
@@ -1,16 +1,75 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { dbService } from 'fbase';
+import { doc, deleteDoc, updateDoc } from 'firebase/firestore';
 
 function Nweet({ nweetObj, isOwner }) {
+    const [editing, setEdting] = useState(false);
+    const [newNweet, setNewNweet] = useState(nweetObj.text);
+
+    const NweetTextRef = doc(dbService, 'nweets', `${nweetObj.id}`);
+
+    const onDeleteClick = async () => {
+        const ok = window.confirm('Are you sure you want to delete this nweet?');
+        
+        if (ok) {
+            await deleteDoc(NweetTextRef);
+        }
+    };
+
+    const onChange = (e) => {
+        const { target: {value} } = e;
+        setNewNweet(value);
+    }
+
+    const onSubmit = async (e) => {
+        e.preventDefault();
+
+        await updateDoc(NweetTextRef, {
+            text: newNweet
+        });
+        
+        setEdting(false);
+    }
+
+    const toggleEditing = () => setEdting(prev => !prev);
+
     return (
         <div>
-            <h4>{nweetObj.text}</h4>
             {
-                isOwner && (
-                    <>
-                        <button>Edit Nweet</button>
-                        <button>Delete Nweet</button>
-                    </>
-                )
+                editing ? 
+                <>
+                    <form onSubmit={onSubmit}>
+                        <input 
+                            type="text" 
+                            placeholder='Edit your nweet'
+                            defaultValue={newNweet}
+                            onChange={onChange}
+                            required
+                        />
+                        <input 
+                            type="submit" 
+                            value="Update Nweet" 
+                        />
+                    </form> 
+                    <button onClick={toggleEditing}>
+                        Cancel
+                    </button>
+                </>:
+                <>
+                    <h4>{nweetObj.text}</h4>
+                    {
+                        isOwner && (
+                            <>
+                                <button onClick={onDeleteClick}>
+                                    Delete Nweet
+                                </button>
+                                <button onClick={toggleEditing}>
+                                    Edit Nweet
+                                </button>   
+                            </>
+                        )
+                    }
+                </>
             }
         </div>
     );
