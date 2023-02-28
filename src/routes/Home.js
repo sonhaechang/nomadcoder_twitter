5e45eea33377392fdcb67f718295382af8a681d4
Son Haechang <sonhaechang@Sonui-MacBookPro.local>
2023-02-28 14:36:20 +0900

    feat: database Setup
    1. firebase database Setup
    2. Home.js form 추가

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Home() {
+    const [nweet, setNweet] = useState('');
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+    }
+
+    const onChange = (e) => {
+        const { target: {value} } = e;
+        setNweet(value);
+    }
+
     return (
-        <span>Home</span>
+        <div>
+            <form onSubmit={onSubmit}>
+                <input 
+                    value={nweet}
+                    onChange={onChange}
+                    type="text" 
+                    placeholder="What's on your mind?" 
+                    maxLength={120} 
+                />
+                <input 
+                    type="submit" 
+                    value='Nweet' 
+                />
+            </form>
+        </div>
     );
 }
 
