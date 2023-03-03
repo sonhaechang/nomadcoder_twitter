982dc710893558591690615b735539f64d407310
Son Haechang <sonhaechang@Sonui-MacBookPro.local>
2023-03-04 00:15:31 +0900

    feat: create file input and show preview image
    1. file upload input jxs 생성 및 이미지 미리보기 img jxs 생성
    2. onFileChange 생성(업로드한 이미지 미리보기 기능)
    3. onClearAttachment 생성(업로드된 이미지 취소 기능 useState, useRef 이용)

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -7,11 +7,12 @@ import {
     query,
     orderBy
 } from 'firebase/firestore';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 function Home({ userObj }) {
     const [nweet, setNweet] = useState('');
     const [nweets, setNweets] = useState([]);
+    const [attachment, setAttachment] = useState();
 
     const onSubmit = async (e) => {
         e.preventDefault();
@@ -28,6 +29,26 @@ function Home({ userObj }) {
     const onChange = (e) => {
         const { target: {value} } = e;
         setNweet(value);
+    };
+
+    const onFileChange = (e) => {
+        const { target: {files} } = e;
+        const theFile = files[0];
+        const reader = new FileReader();
+
+        reader.onloadend = (finishedEvent) => {
+            const { currentTarget: {result} } = finishedEvent;
+            setAttachment(result);
+        };
+
+        reader.readAsDataURL(theFile);
+    };
+
+    const fileInput = useRef();
+    
+    const onClearAttachment = () => {
+        setAttachment(null);
+        fileInput.current.value = null;
     };
 
     useEffect(() => {
@@ -56,6 +77,25 @@ function Home({ userObj }) {
                     placeholder="What's on your mind?" 
                     maxLength={120} 
                 />
+                <input 
+                    type="file" 
+                    accept='image/*'
+                    ref={fileInput}
+                    onChange={onFileChange} 
+                />
+                {
+                    attachment && 
+                    <div>
+                        <img 
+                            src={attachment} 
+                            alt='image_preview' 
+                            style={{ width: '50px', height: '50px' }} 
+                        />
+                        <button onClick={onClearAttachment}>
+                            Clear
+                        </button>
+                    </div>
+                }
                 <input 
                     type="submit" 
                     value='Nweet' 
